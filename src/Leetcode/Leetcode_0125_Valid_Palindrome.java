46b10836dca5145d2c5c1cbb718e9fdef0673032
Chudong <cpan65@gatech.edu>
2020-12-06 20:51:22 -0500

    Leetcode_0125_Valid_Palindrome

---
+++ src/Leetcode/Leetcode_0125_Valid_Palindrome.java
@@ -0,0 +1,34 @@
+package Leetcode;
+
+public class Leetcode_0125_Valid_Palindrome {
+    public static boolean isPalindrome(String s) {
+        int n = s.length();
+        int left = 0, right = n - 1;
+        while(left < right) {
+            while(left < right && !isValidChar(s.charAt(left))) {
+                left++;
+            }
+            while(left < right && !isValidChar(s.charAt(right))) {
+                right--;
+            }
+            if(!isEqualChar(s.charAt(left), s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static boolean isValidChar(char a) {
+        return Character.isAlphabetic(a) || Character.isDigit(a);
+    }
+
+    public static boolean isEqualChar(char c1, char c2) {
+        return Character.toLowerCase(c1) == Character.toLowerCase(c2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("A man, a plan, a canal: Panama"));
+    }
+}
